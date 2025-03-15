5d1f9ab7f8c1f96da27b5963cbe1787b023ada56
Nkwenti-Severian-Ndongtsop <180976800+Nkwenti-Severian-Ndongtsop@users.noreply.github.com>
2025-03-15 10:00:17 +0100

    chore(workspace): added a cargo workspace

--- rust-hello-server/src/main.rs
+++ rust-hello-server/src/main.rs
@@ -1,22 +1,20 @@
+use crate::routers::{get_routes::display, get_routes::index, post_routes::upload};
 use axum::{routing::get, Router};
 use tokio::net::TcpListener;
-use crate::routers::{get_routes::display, post_routes::upload, get_routes::index};
-
-
 
 fn router() -> Router {
     Router::new()
-    .route("/hello", get(display))
-    .route("/", get(index).post(upload))
+        .route("/hello", get(display))
+        .route("/", get(index).post(upload))
 }
 
 #[tokio::main]
 async fn main() {
-    let addr = "127.0.0.1:5555";
-    
-    let listener = TcpListener::bind(addr).await.unwrap();
+    let addr = "127.0.0.1:8888";
+
+    let listener = TcpListener::bind(addr).await.expect("Failed to read address");
     println!("Server running on: http://{}", addr);
-    
+
     axum::serve(listener, router()).await.unwrap();
 }
 
