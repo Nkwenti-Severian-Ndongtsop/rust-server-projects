076a4b3a8c1f12982c0b22ece585164db5ec48b3
Nkwenti-Severian-Ndongtsop <180976800+Nkwenti-Severian-Ndongtsop@users.noreply.github.com>
2025-03-20 16:11:12 +0100

    chore(weather/main): improve cli parsing for ddefault

--- weather-app/src/main.rs
+++ weather-app/src/main.rs
@@ -1,23 +1,9 @@
 use std::env;
 
 use reqwest::Error;
-use serde::Deserialize;
+use structs::WeatherData;
 
-#[derive(Deserialize)]
-struct Weather {
-    description: String,
-}
-
-#[derive(Deserialize)]
-struct Main {
-    temp: f64,
-}
-
-#[derive(Deserialize)]
-struct WeatherData {
-    weather: Vec<Weather>,
-    main: Main,
-}
+mod structs;
 
 async fn fetch_weather(location: &str) -> Result<(), Error> {
     let api_key = "ec46e688303fc6fda12f9f5c46c59614";
@@ -29,7 +15,7 @@ async fn fetch_weather(location: &str) -> Result<(), Error> {
     let response = reqwest::get(&url).await?.json::<WeatherData>().await?;
 
     println!(
-        "The weather in {} is {} \nwith a temperature of {}°C",
+        "The weather in {} is {} \nwith a temperature of {}°C\n",
         location,
         response.weather[0].description,
         response.main.temp - 273.15
@@ -38,11 +24,37 @@ async fn fetch_weather(location: &str) -> Result<(), Error> {
     Ok(())
 }
 
-#[tokio::main]
-async fn main() {
-    let args: Vec<String> = env::args().collect();
+async fn geolocation() -> Result<(), Error> {
+    let url = "http://ip-api.com/json/";
+    let response = reqwest::get(url)
+        .await?
+        .json::<structs::GeoLocation>()
+        .await?;
+    println!(
+        "Your Hosted on: {}
+    With a Timezone of: {}
+    In the City: {}
+    Your ISP is: {}",
+        response.country, response.timezone, response.city, response.isp
+    );
 
-    if args.len() < 2 {
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let url = "http://ip-api.com/json/";
+
+    let default_loc = reqwest::get(url)
+        .await?
+        .json::<structs::GeoLocation>()
+        .await?;
+    let mut args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        let location = default_loc.country.clone();
+        args.push(location);
+    } else {
         eprintln!("Usage: weather-app <city>");
         std::process::exit(1);
     }
@@ -51,4 +63,11 @@ async fn main() {
         eprintln!("Error fetching weather: {}", e);
         eprintln!("Might be invalid city name or network error");
     }
+
+    if let Err(e) = geolocation().await {
+        eprintln!("Error fetching your data: {}", e);
+        eprintln!("Might be network issues");
+    }
+
+    Ok(())
 }
