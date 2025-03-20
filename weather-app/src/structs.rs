cf64513860f399229dcbdb565f0bd8f51de8fbf3
Nkwenti-Severian-Ndongtsop <180976800+Nkwenti-Severian-Ndongtsop@users.noreply.github.com>
2025-03-20 16:10:31 +0100

    chore(weather/struct): added some attributes for better response

--- weather-app/src/structs.rs
+++ weather-app/src/structs.rs
@@ -22,3 +22,11 @@ pub struct WeatherResponse {
     pub temperature: f32,
     pub description: String,
 }
+
+#[derive(Deserialize)]
+pub struct GeoLocation {
+    pub country: String,
+    pub timezone: String,
+    pub isp: String,
+    pub city: String 
+}
