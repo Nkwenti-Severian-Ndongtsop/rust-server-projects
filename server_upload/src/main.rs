ce0d68fbdb3fcb2cc0aa901f38e70da7b5f72dc7
Nkwenti-Severian-Ndongtsop <180976800+Nkwenti-Severian-Ndongtsop@users.noreply.github.com>
2025-03-21 07:44:08 +0100

    feat(server_upload/main): handle multiple request

--- server_upload/src/main.rs
+++ server_upload/src/main.rs
@@ -9,29 +9,30 @@ use std::io::Read;
 #[command(version = "1.0", about = "Uploads a file to the server")]
 struct Args {
     /// Path to the file to upload
-    file_path: String,
+    file_path: Vec<String>,
 }
 
 fn main() {
     let args = Args::parse();
     let server_url = "http://localhost:8000/upload";
 
-    // Read the file
-    let mut file = File::open(&args.file_path).expect("Failed to open file");
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).expect("Failed to read file");
+    // Read the ca
+    for file_path in &args.file_path {
+        let mut file = File::open(file_path).expect("Failed to open file");
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer).expect("Failed to read file");
 
-    // Create a multipart form
-    let part = Part::bytes(buffer).file_name(args.file_path.clone());
-    let form = Form::new().part("file", part);
+        // Create a multipart form
+        let part = Part::bytes(buffer).file_name(file_path.clone());
+        let form = Form::new().part("file", part);
 
-    // Send the file
-    let client = Client::new();
-    let response = client
-        .post(server_url)
-        .multipart(form)
-        .send()
-        .expect("Failed to send request");
-
-    println!("Server Response: {:?}", response.text().unwrap());
+        // Send the file
+        let client = Client::new();
+        let response = client
+            .post(server_url)
+            .multipart(form)
+            .send()
+            .expect("Failed to send request");
+        println!("Server Response: {:?}", response.text().unwrap());
+    }
 }
