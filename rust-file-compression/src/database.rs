494eb5af1b89f77410352703d80adc2e553fec37
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-04-01 07:44:24 +0100

    chore(file): added pgpool for status update

---
+++ rust-file-compression/src/database.rs
@@ -0,0 +1,26 @@
+pub async fn insert_user(
+    pool1: &sqlx::PgPool,
+    pool2: &sqlx::PgPool,
+    // pool3: &sqlx::PgPool,
+    file_path: &str,
+) -> u64 {
+    let compressed_file = format!("{}.gz", file_path);
+    // Blocking database operation inside an async function (inefficient)
+    let query = "INSERT INTO files (file_path, compressed_file) VALUES ($1, $2)";
+    sqlx::query(query)
+        .bind(file_path)
+        .bind(compressed_file)
+        .execute(pool1)
+        .await
+        .unwrap();
+    let id = "SELECT id FROM files WHERE file_path=$1";
+
+    let result = sqlx::query(id)
+        .bind(file_path)
+        .execute(pool2)
+        .await
+        .unwrap();
+    let id = result.rows_affected();
+    println!("task_id: {}", id);
+    id
+}
