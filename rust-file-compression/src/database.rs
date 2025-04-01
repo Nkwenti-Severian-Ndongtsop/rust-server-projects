da73a54d8151e74ec86d38856b4d760646b9745f
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-04-01 23:19:04 +0100

    chore(file): added cli

--- rust-file-compression/src/database.rs
+++ rust-file-compression/src/database.rs
@@ -16,3 +16,28 @@ pub async fn insert_user(pool1: &sqlx::PgPool, pool2: &sqlx::PgPool, file_path: 
         .unwrap();
     result.rows_affected()
 }
+
+use sqlx::PgPool;
+
+#[derive(Debug, sqlx::Type)]
+#[sqlx(type_name = "file_status", rename_all = "snake_case")] // Ensure the enum matches the DB's case convention
+pub enum FileStatus {
+    Pending,
+    Completed,
+    Failed,
+}
+
+pub async fn update_state(pool: &PgPool, file_id: i32, new_status: FileStatus) -> Result<(), sqlx::Error> {
+    sqlx::query(
+        "UPDATE files SET file_state = $1 WHERE id = $2"
+    )
+    .bind(new_status)  // Use `.to_string()` to bind the enum as a string
+    .bind(file_id)
+    .execute(pool)
+    .await?;
+    
+    println!("Successfully updated the value for file ID: {}", file_id);
+    
+    Ok(())
+}
+
