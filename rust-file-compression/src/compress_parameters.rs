e4248f034ceaddf4b74f5327959c510ace275b44
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-04-01 08:10:43 +0100

    chore(file): fix indentation

--- rust-file-compression/src/compress_parameters.rs
+++ rust-file-compression/src/compress_parameters.rs
@@ -1,11 +1,9 @@
-
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 pub async fn param() -> Pool<Postgres> {
-    let pool = PgPoolOptions::new()
+    PgPoolOptions::new()
         .max_connections(5)
         .connect("postgresql://postgres:mysecretpassword@10.39.78.149:5432/upload")
         .await
-        .unwrap();
-    pool
+        .unwrap()
 }
