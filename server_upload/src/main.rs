7ee62de8fdc1db43fcb4b3a8850a790ef32de972
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-04-01 20:53:32 +0100

    chore(file): added cli

--- server_upload/src/main.rs
+++ server_upload/src/main.rs
@@ -1,28 +1,34 @@
 use clap::{Arg, Command};
-use reqwest::blocking::{Client, multipart::{Form, Part}};
+use reqwest::blocking::{
+    multipart::{Form, Part},
+    Client,
+};
 use std::{fs::File, io::Read};
 
 fn main() {
-
     let matches = Command::new("File Uploader")
-        .arg(Arg::new("compress")
-            .short('c')
-            .num_args(1..)
-            .help("Files to compress & upload"))
-        .arg(Arg::new("method")
-            .short('m')
-            .default_value("default")
-            .help("Compression method: best, fast, or default"))
+        .arg(
+            Arg::new("compress")
+                .short('c')
+                .num_args(1..)
+                .help("Files to compress & upload"),
+        )
+        .arg(
+            Arg::new("method")
+                .short('m')
+                .default_value("default")
+                .help("Compression method: best, fast, or default"),
+        )
         .get_matches();
 
-
-    let files: Vec<&str> = matches.get_many::<String>("compress")
+    let files: Vec<&str> = matches
+        .get_many::<String>("compress")
         .unwrap_or_default()
         .map(String::as_str)
         .collect();
 
     let method = matches.get_one::<String>("method").unwrap();
-    
+
     let server_url = "http://localhost:8000/upload";
     let client = Client::new();
 
@@ -32,14 +38,16 @@ fn main() {
                 let mut buffer = Vec::new();
                 if file.read_to_end(&mut buffer).is_ok() {
                     let filename = file_path.split('/').last().unwrap_or("unknown_file");
-                    
+
                     let form = Form::new()
                         .part("file", Part::bytes(buffer).file_name(filename.to_string()))
-                        .text("method", method.to_string()); 
-                    
+                        .text("method", method.to_string());
+
                     match client.post(server_url).multipart(form).send() {
                         Ok(response) => match response.text() {
-                            Ok(text) => println!("Uploaded {} - Server Response: {}", filename, text),
+                            Ok(text) => {
+                                println!("Uploaded {} - Server Response: {}", filename, text)
+                            }
                             Err(_) => println!("Failed to read server response."),
                         },
                         Err(err) => println!("Request failed for {}: {:?}", filename, err),
