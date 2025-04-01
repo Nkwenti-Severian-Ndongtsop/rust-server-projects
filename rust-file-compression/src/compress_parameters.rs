04497286dd933ca450ea8a7988ec6c99a5b5843b
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-04-01 20:32:15 +0100

    chore(file): added cli

--- rust-file-compression/src/compress_parameters.rs
+++ rust-file-compression/src/compress_parameters.rs
@@ -3,7 +3,7 @@ use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 pub async fn param() -> Pool<Postgres> {
     PgPoolOptions::new()
         .max_connections(5)
-        .connect("postgresql://postgres:mysecretpassword@10.39.78.149:5432/upload")
+        .connect("postgresql://halamadrid:mysecretpassword@localhost:5432/server_db")
         .await
         .unwrap()
 }
