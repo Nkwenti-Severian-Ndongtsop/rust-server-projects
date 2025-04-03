f0f8e77d2c5706daa10507b43b4cff89a11e4404
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-04-03 08:16:48 +0100

    chore(file): added .env setup

--- rust-file-compression/src/compress_parameters.rs
+++
@@ -1,9 +0,0 @@
-use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
-
-pub async fn param() -> Pool<Postgres> {
-    PgPoolOptions::new()
-        .max_connections(5)
-        .connect("postgresql://halamadrid:mysecretpassword@localhost:5432/server_db")
-        .await
-        .unwrap()
-}
